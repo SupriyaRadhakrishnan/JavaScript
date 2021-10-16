803b557a679ddae958b6b33c3f5373d2e22461fe
SupriyaRadhakrishnan <supriya7458@gmail.com>
2021-10-15 20:20:35 -0400

    commits

--- ArithmeticOperators.js
+++ ArithmeticOperators.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let num1 = 4;
 let num2 = 2;
 let result = num1 + num2;
@@ -15,7 +14,7 @@ let boo2 = true;
 let booresult = boo1 + boo2;
 console.log(booresult); //Prints 2.Considers true as 1..so 1+1
 
-//Short hab=nd operators
+//Short hand operators
 let a = 5;
 a += 5
 console.log(a);
@@ -26,34 +25,3 @@ console.log(a);
 
 let b = 3;
 console.log(Math.pow(b,3)); // or we can say console.log(4 ** 3) which also means power
-
-=======
-let num1 = 4;
-let num2 = 2;
-let result = num1 + num2;
-console.log(result);
-result= num1-num2;
-console.log(result);
-result= num1*num2;
-console.log(result);
-result= num1/num2;
-console.log(result);result= num1%num2;
-console.log(result);
-let boo1 = true;
-let boo2 = true;
-let booresult = boo1 + boo2;
-console.log(booresult); //Prints 2.Considers true as 1..so 1+1
-
-//Short hab=nd operators
-let a = 5;
-a += 5
-console.log(a);
-++a;//can also be --a
-console.log(a);
-a++;//can also be a--
-console.log(a);
-
-let b = 3;
-console.log(Math.pow(b,3)); // or we can say console.log(4 ** 3) which also means power
-
->>>>>>> d2123cdef939d2bea5aa3dc1bc310e1c7b4ff008
